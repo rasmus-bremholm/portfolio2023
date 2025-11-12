08d32a1a138acc860f67ac8e1255af66a9877901
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-11-12 08:59:04 +0100

    Change to MUI

--- src/app/components/surfaces/navbar/Navbar.tsx
+++ src/app/components/surfaces/navbar/Navbar.tsx
@@ -1,22 +1,24 @@
 "use client";
 import { useState } from "react";
-import { Menu, Close } from "@mui/icons-material";
+import { Menu, Close, Home, Book, AccountTree, ConnectWithoutContact } from "@mui/icons-material";
 import Link from "next/link";
-import { motion, AnimatePresence, animate, easeOut, easeInOut } from "framer-motion";
-import LogoButton from "../../buttons/logoButton";
+import { Box, Button, IconButton, Drawer, List, ListItem, ListItemButton, ListItemText, ListItemIcon, Divider } from "@mui/material";
+import LogoButton from "../../buttons/LogoButton";
 
 interface MenuItems {
 	title: string;
 	link: string;
+	icon: React.ReactNode;
 }
 
 export default function Navbar() {
 	const [menuOpen, setMenuOpen] = useState(false); // Change to false in prod!
 
 	const menuItems: MenuItems[] = [
-		{ title: "Blog", link: "/blog" },
-		{ title: "Projects", link: "/projects" },
-		{ title: "Contact", link: "/contact" },
+		{ title: "Home", link: "/", icon: <Home /> },
+		{ title: "Blog", link: "/blog", icon: <Book /> },
+		{ title: "Projects", link: "/projects", icon: <AccountTree /> },
+		{ title: "Contact", link: "/contact", icon: <ConnectWithoutContact /> },
 	];
 
 	const toggleMenu = () => {
@@ -24,48 +26,46 @@ export default function Navbar() {
 	};
 
 	return (
-		<nav>
-			<div id='Mobile Menu'>
-				<div id='Logo Section'>
-					<LogoButton />
-				</div>
-				<div id='Hamburger Container'>
-					<button onClick={toggleMenu} aria-label={menuOpen ? "Close Menu" : "Open Menu"} aria-expanded={menuOpen} aria-controls='mobile-menu'>
+		<Box component='nav'>
+			<Box id='Mobile Menu' sx={{ display: { xs: "flex", md: "none", alignItems: "center" } }}>
+				<Box id='Logo Section'>
+					<Link href={"/"}>
+						<LogoButton />
+					</Link>
+				</Box>
+				<Box id='Hamburger Container'>
+					<IconButton onClick={toggleMenu} aria-label={menuOpen ? "Close Menu" : "Open Menu"} aria-expanded={menuOpen} aria-controls='mobile-menu'>
 						{menuOpen ?
 							<Close />
 						:	<Menu color='secondary' />}
-					</button>
-				</div>
-				{menuOpen && (
-					<AnimatePresence>
-						<motion.div
-							id='Hamburger Menu'
-							role='menu'
-							aria-label='mobile-menu'
-							initial={{ height: 0, opacity: 0 }}
-							animate={{ height: "auto", opacity: 1 }}
-							exit={{ height: 0, opacity: 0 }}
-							transition={{ duration: 0.2, ease: easeInOut }}
-							style={{ overflow: "hidden" }}>
-							{menuItems.map((item, index) => (
-								<Link key={index} href={item.link} onClick={() => setMenuOpen(false)}>
-									{item.title}
-								</Link>
-							))}
-						</motion.div>
-					</AnimatePresence>
-				)}
-			</div>
-			<div id='Desktop Menu'>
-				<div id='Logo Section'>
-					<LogoButton />
-				</div>
+					</IconButton>
+				</Box>
+
+				<Drawer id='Hamburger Menu' role='menu' aria-label='mobile-menu' anchor='top' open={menuOpen} onClose={() => setMenuOpen(false)}>
+					<List>
+						{menuItems.map((item, index) => (
+							<ListItem key={index}>
+								<ListItemButton component={Link} href={item.link} onClick={() => setMenuOpen(false)}>
+									<ListItemIcon>{item.icon}</ListItemIcon>
+									<ListItemText primary={item.title} />
+								</ListItemButton>
+							</ListItem>
+						))}
+					</List>
+				</Drawer>
+			</Box>
+			<Box id='Desktop Menu' sx={{ display: { xs: "none", md: "flex", alignItems: "center" } }}>
+				<Box id='Logo Section'>
+					<Link href={"/"}>
+						<LogoButton />
+					</Link>
+				</Box>
 				{menuItems.map((item, index) => (
 					<Link key={index} href={item.link}>
 						{item.title}
 					</Link>
 				))}
-			</div>
-		</nav>
+			</Box>
+		</Box>
 	);
 }
