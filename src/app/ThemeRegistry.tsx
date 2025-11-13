9088d3c84855e40b816a16bf16abf3d54bef9933
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-11-14 00:03:41 +0100

    Navbar semi finished

--- src/app/ThemeRegistry.tsx
+++ src/app/ThemeRegistry.tsx
@@ -1,17 +1,35 @@
-'use client';
-import * as React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import theme from '@/theme';
+"use client";
+import * as React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { Box } from "@mui/material";
+import theme from "@/theme";
 
 export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
-  return (
-    <AppRouterCacheProvider>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </AppRouterCacheProvider>
-  );
+	return (
+		<AppRouterCacheProvider>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				{/* Noise texture overlay */}
+				<Box
+					sx={{
+						position: "fixed",
+						top: 0,
+						left: 0,
+						width: "100%",
+						height: "100%",
+						backgroundImage: 'url("/otis-redding.png")',
+						backgroundRepeat: "repeat",
+						backgroundSize: "96px",
+						opacity: 0.15,
+						mixBlendMode: "normal",
+						pointerEvents: "none",
+						zIndex: 9999,
+					}}
+				/>
+				{children}
+			</ThemeProvider>
+		</AppRouterCacheProvider>
+	);
 }
--- src/app/components/surfaces/navbar/Navbar.tsx
+++ src/app/components/surfaces/navbar/Navbar.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { Menu, Close, Home, Book, AccountTree, ConnectWithoutContact } from "@mui/icons-material";
 import Link from "next/link";
-import { Box, Button, IconButton, Drawer, List, ListItem, ListItemButton, ListItemText, ListItemIcon, Divider } from "@mui/material";
+import { AppBar, Toolbar, Box, Button, IconButton, Drawer, List, ListItem, ListItemButton, ListItemText, ListItemIcon, Container } from "@mui/material";
 import LogoButton from "../../buttons/LogoButton";
 
 interface MenuItems {
@@ -12,7 +12,7 @@ interface MenuItems {
 }
 
 export default function Navbar() {
-	const [menuOpen, setMenuOpen] = useState(false); // Change to false in prod!
+	const [menuOpen, setMenuOpen] = useState(false);
 
 	const menuItems: MenuItems[] = [
 		{ title: "Home", link: "/", icon: <Home /> },
@@ -26,46 +26,122 @@ export default function Navbar() {
 	};
 
 	return (
-		<Box component='nav'>
-			<Box id='Mobile Menu' sx={{ display: { xs: "flex", md: "none", alignItems: "center" } }}>
-				<Box id='Logo Section'>
-					<Link href={"/"}>
-						<LogoButton />
-					</Link>
-				</Box>
-				<Box id='Hamburger Container'>
-					<IconButton onClick={toggleMenu} aria-label={menuOpen ? "Close Menu" : "Open Menu"} aria-expanded={menuOpen} aria-controls='mobile-menu'>
+		<AppBar position='static' component='nav' elevation={0} sx={{ backgroundColor: "background.default" }}>
+			<Container maxWidth='lg'>
+				<Toolbar disableGutters sx={{ justifyContent: "space-between", minHeight: { xs: 64, md: 70 } }}>
+					{/* Logo Section */}
+					<Box sx={{ display: "flex", alignItems: "center" }}>
+						<Link href='/' style={{ display: "flex", alignItems: "center", textDecoration: "none" }}>
+							<LogoButton />
+						</Link>
+					</Box>
+
+					{/* Desktop Menu */}
+					<Box sx={{ display: { xs: "none", md: "flex" }, gap: 3, alignItems: "center" }}>
+						{menuItems.map((item, index) => (
+							<Link
+								key={index}
+								href={item.link}
+								style={{
+									textDecoration: "none",
+									color: "inherit",
+									position: "relative",
+									fontSize: "1rem",
+									fontWeight: 400,
+									padding: "4px 0",
+								}}>
+								<Box
+									component='span'
+									sx={{
+										color: "text.primary",
+										position: "relative",
+										"&:hover": {
+											color: "primary.main",
+										},
+										"&::after": {
+											content: '""',
+											position: "absolute",
+											bottom: -4,
+											left: 0,
+											width: 0,
+											height: "2px",
+											backgroundColor: "primary.main",
+											transition: "width 0.3s ease",
+										},
+										"&:hover::after": {
+											width: "100%",
+										},
+									}}>
+									{item.title}
+								</Box>
+							</Link>
+						))}
+					</Box>
+
+					{/* Mobile Menu Button */}
+					<IconButton
+						onClick={toggleMenu}
+						aria-label={menuOpen ? "Close Menu" : "Open Menu"}
+						aria-expanded={menuOpen}
+						aria-controls='mobile-menu'
+						sx={{
+							display: { xs: "flex", md: "none" },
+							color: menuOpen ? "primary.main" : "text.primary",
+						}}>
 						{menuOpen ?
 							<Close />
-						:	<Menu color='secondary' />}
+						:	<Menu />}
 					</IconButton>
-				</Box>
 
-				<Drawer id='Hamburger Menu' role='menu' aria-label='mobile-menu' anchor='top' open={menuOpen} onClose={() => setMenuOpen(false)}>
-					<List>
-						{menuItems.map((item, index) => (
-							<ListItem key={index}>
-								<ListItemButton component={Link} href={item.link} onClick={() => setMenuOpen(false)}>
-									<ListItemIcon>{item.icon}</ListItemIcon>
-									<ListItemText primary={item.title} />
-								</ListItemButton>
-							</ListItem>
-						))}
-					</List>
-				</Drawer>
-			</Box>
-			<Box id='Desktop Menu' sx={{ display: { xs: "none", md: "flex", alignItems: "center" } }}>
-				<Box id='Logo Section'>
-					<Link href={"/"}>
-						<LogoButton />
-					</Link>
-				</Box>
-				{menuItems.map((item, index) => (
-					<Link key={index} href={item.link}>
-						{item.title}
-					</Link>
-				))}
-			</Box>
-		</Box>
+					{/* Mobile Drawer */}
+					<Drawer
+						id='mobile-menu'
+						anchor='top'
+						open={menuOpen}
+						onClose={() => setMenuOpen(false)}
+						sx={{
+							display: { xs: "block", md: "none" },
+							"& .MuiDrawer-paper": {
+								backgroundColor: "background.default",
+								borderBottom: "1px solid",
+								borderColor: "divider",
+								mt: 8,
+							},
+						}}>
+						<List sx={{ py: 2 }}>
+							{menuItems.map((item, index) => (
+								<ListItem key={index} disablePadding>
+									<ListItemButton
+										component={Link}
+										href={item.link}
+										onClick={() => setMenuOpen(false)}
+										sx={{
+											py: 1.5,
+											"&:hover": {
+												backgroundColor: "rgba(3, 166, 120, 0.08)",
+												"& .MuiListItemIcon-root": {
+													color: "primary.main",
+												},
+												"& .MuiListItemText-primary": {
+													color: "primary.main",
+												},
+											},
+										}}>
+										<ListItemIcon sx={{ color: "text.primary", minWidth: 40 }}>{item.icon}</ListItemIcon>
+										<ListItemText
+											primary={item.title}
+											primaryTypographyProps={{
+												fontSize: "1.1rem",
+												fontWeight: 400,
+											}}
+										/>
+									</ListItemButton>
+								</ListItem>
+							))}
+						</List>
+					</Drawer>
+				</Toolbar>
+			</Container>
+		</AppBar>
 	);
 }
