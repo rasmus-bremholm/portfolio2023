ee79264c93880a723c9cda306f34ec676613dc55
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-11-17 08:59:18 +0100

    Header work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,35 @@
 import HomepageSection from "./components/surfaces/HomepageSection";
 import { getAllContentSections } from "../../sanity/lib/client";
 import type { ContentSection } from "@/types/sanity/homepage";
+import { Box, Container, Typography } from "@mui/material";
 
 export default async function Home() {
 	const sections: ContentSection[] = await getAllContentSections();
 
 	return (
-		<main>
+		<Container component={"main"}>
+			<Box component={"header"} sx={{ display: "flex", flexDirection: "column", minHeight: "50svh", justifyContent: "center", maxWidth: "80%" }}>
+				<Typography
+					variant='h1'
+					sx={{
+						letterSpacing: "0.02em",
+						lineHeight: 1,
+						"& span": {
+							background: "linear-gradient(90deg,#03a678 0%, rgba(237, 221, 83, 1) 100%);",
+							WebkitBackgroundClip: "text",
+							WebkitTextFillColor: "transparent",
+							backgroundClip: "text",
+						},
+					}}>
+					Hello, I'm <span>Rasmus Bremholm</span>
+				</Typography>
+				<Typography variant='h3' component={"h2"}>
+					Fullstack Developer
+				</Typography>
+				<Typography sx={{ fontSize: "1.5rem" }}>Exploring areas where tech and artistry meet.</Typography>
+			</Box>
 			{sections &&
 				sections.map((section) => <HomepageSection key={section._id} title={section.title} content={section.content} alignment={section.alignment} />)}
-		</main>
+		</Container>
 	);
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,81 +1,91 @@
-'use client';
-import { Bebas_Neue, Lato } from 'next/font/google';
-import { createTheme } from '@mui/material/styles';
+"use client";
+import { Bebas_Neue, Lato } from "next/font/google";
+import { createTheme, Components } from "@mui/material/styles";
 
 export const bebasNeue = Bebas_Neue({
-  subsets: ['latin'],
-  weight: '400',
-  display: 'swap',
+	subsets: ["latin"],
+	weight: "400",
+	display: "swap",
 });
 
 export const lato = Lato({
-  subsets: ['latin'],
-  weight: ['300', '400', '700'],
-  display: 'swap',
+	subsets: ["latin"],
+	weight: ["300", "400", "700"],
+	display: "swap",
 });
 
+const typography = {
+	fontFamily: lato.style.fontFamily,
+	body1: {
+		color: "#898989", // text.secondary value
+	},
+	body2: {
+		color: "#898989",
+	},
+	h1: {
+		fontFamily: bebasNeue.style.fontFamily,
+		color: "#FAFAFA",
+	},
+	h2: {
+		fontFamily: bebasNeue.style.fontFamily,
+	},
+	h3: {
+		fontFamily: bebasNeue.style.fontFamily,
+	},
+	h4: {
+		fontFamily: bebasNeue.style.fontFamily,
+	},
+	h5: {
+		fontFamily: bebasNeue.style.fontFamily,
+	},
+	h6: {
+		fontFamily: bebasNeue.style.fontFamily,
+	},
+};
+
+const components: Components = {
+	MuiButton: {
+		styleOverrides: {
+			root: {
+				textTransform: "none",
+			},
+		},
+	},
+};
+
 const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#03a678',
-    },
-    secondary: {
-      main: '#1c6bba',
-    },
-    error: {
-      main: '#bf2012',
-    },
-    warning: {
-      main: '#ffa200',
-    },
-    success: {
-      main: '#1a6b31',
-    },
-    info: {
-      main: '#0b66d5',
-    },
-    background: {
-      default: '#121212',
-      paper: '#171717',
-    },
-    text: {
-      primary: '#fafafa',
-      secondary: '#898989',
-      disabled: '#686666',
-    },
-    divider: 'rgba(255, 255, 255, 0.12)',
-  },
-  typography: {
-    fontFamily: lato.style.fontFamily,
-    h1: {
-      fontFamily: bebasNeue.style.fontFamily,
-    },
-    h2: {
-      fontFamily: bebasNeue.style.fontFamily,
-    },
-    h3: {
-      fontFamily: bebasNeue.style.fontFamily,
-    },
-    h4: {
-      fontFamily: bebasNeue.style.fontFamily,
-    },
-    h5: {
-      fontFamily: bebasNeue.style.fontFamily,
-    },
-    h6: {
-      fontFamily: bebasNeue.style.fontFamily,
-    },
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          textTransform: 'none',
-        },
-      },
-    },
-  },
+	typography,
+	palette: {
+		mode: "dark",
+		primary: {
+			main: "#03a678",
+		},
+		secondary: {
+			main: "#1c6bba",
+		},
+		error: {
+			main: "#bf2012",
+		},
+		warning: {
+			main: "#ffa200",
+		},
+		success: {
+			main: "#1a6b31",
+		},
+		info: {
+			main: "#0b66d5",
+		},
+		background: {
+			default: "#121212",
+			paper: "#171717",
+		},
+		text: {
+			primary: "#fafafa",
+			secondary: "#898989",
+		},
+		divider: "rgba(255, 255, 255, 0.12)",
+	},
+	components,
 });
 
 export default theme;
