7571e8e5e9101749e8e2efbebbf6b00fea68fa5d
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-11-18 10:53:13 +0100

    Sanity Image test

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,6 +3,9 @@ import { blogPostQuery } from "@/sanity/lib";
 import { Typography, Box, Container } from "@mui/material";
 import { PortableText } from "next-sanity";
 import { renderComponents } from "@/sanity/lib/renderComponents";
+import { urlFor } from "@/sanity/lib/sanityImageUrl";
+import { SanityImageObject } from "@sanity/image-url/lib/types/types";
+import Image from "next/image";
 
 export default async function BlogPost({ params }: { params: Promise<{ slug: string }> }) {
 	const { slug } = await params;
@@ -13,12 +16,19 @@ export default async function BlogPost({ params }: { params: Promise<{ slug: str
 	return (
 		<Container maxWidth='md'>
 			<Box>
-				<Typography variant='h1'>{post.title}</Typography>
+				{post.featuredImage && (
+					<Image
+						src={urlFor(post.featuredImage).width(800).height(400).dpr(2).url()}
+						height={800}
+						width={400}
+						alt={post.featuredImage.alt}
+						placeholder='blur'
+						blurDataURL={urlFor(post.featuredImage).width(24).height(24).blur(10).url()}></Image>
+				)}
 			</Box>
 			<Box>
-				<Typography>
-					<PortableText value={post.content} components={renderComponents} />
-				</Typography>
+				<Typography variant='h1'>{post.title}</Typography>
+				<PortableText value={post.content} components={renderComponents} />
 			</Box>
 		</Container>
 	);
