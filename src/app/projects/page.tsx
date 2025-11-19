192f9c53cd84eca2c9692c429c976c4f31e10c80
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-11-19 10:52:20 +0100

    project page header

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,13 +2,16 @@ import { Container, Box } from "@mui/material";
 import { client } from "@/sanity/lib/client";
 import { projectsQuery } from "@/sanity/lib";
 import ProjectsList from "./components/ProjectsList";
+import ProjectPageHeader from "../components/surfaces/header/ProjectPageHeader";
 
 export default async function Projects() {
 	const posts = await client.fetch(projectsQuery);
 
 	return (
 		<Container component='main' maxWidth='md'>
-			<Box></Box>
+			<Box>
+				<ProjectPageHeader />
+			</Box>
 			<Box>
 				<ProjectsList posts={posts} />
 			</Box>
