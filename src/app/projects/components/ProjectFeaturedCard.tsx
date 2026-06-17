2f949d2ddabfa4292670515015996562245f4b8b
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-06-17 15:29:28 +0200

    Update ProjectFeaturedCard.tsx

--- src/app/projects/components/ProjectFeaturedCard.tsx
+++ src/app/projects/components/ProjectFeaturedCard.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 import type { ProjectPreview } from "@/types/sanity/projectpage";
 
@@ -10,12 +10,24 @@ export default function ProjectFeaturedCard({ project }: ProjectFeaturedCardProp
 	const { featuredImage, title } = project;
 	return (
 		<Box sx={{ display: "grid", gridTemplateColumns: "5fr 4fr", border: "1px solid", borderColor: "divider" }}>
-			<Box id='media' sx={{ borderRight: "1px solid", borderColor: "divider", position: "relative", minHeight: 400 }}>
-				{featuredImage ? (
-					<Image src={featuredImage.asset.url} alt={featuredImage.alt ?? title} fill style={{ objectFit: "cover" }} />
-				) : (
-					<Box sx={{ width: "100%", height: "100%", bgcolor: "action.hover" }} />
-				)}
+			<Box
+				id='media'
+				sx={{
+					borderRight: "1px solid",
+					borderColor: "divider",
+					position: "relative",
+					minHeight: 400,
+					p: 4,
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}>
+				<Box sx={{ position: "absolute", top: 12, left: 12, border: "1px solid", borderColor: "primary.main", px: 2, py: 0.5 }}>
+					<Typography variant='overline' sx={{ color: "primary.main" }}>
+						{"// Featured"}
+					</Typography>
+				</Box>
+				{featuredImage && <Image src={featuredImage?.asset.url} alt={featuredImage?.alt ?? title} height={400} width={400} />}
 			</Box>
 			<Box id='content'>Content</Box>
 		</Box>
