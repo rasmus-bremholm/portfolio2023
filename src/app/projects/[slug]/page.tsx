fd7d5fc68821b2cf3c70430e238425bbd6c6ba6a
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-08-27 11:48:17 +0200

    Major strides

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,9 +1,50 @@
-import { client } from "@/sanity/lib/client";
-import { projectPostQuery } from "@/sanity/lib/queries";
-import type { Project } from "@/types/sanity/projectpage";
-import { Container } from "@mui/material";
+import { Container, Box, Typography } from "@mui/material";
 import { notFound } from "next/navigation";
+import { fetchProjectBySlug } from "@/sanity/lib/client";
+import formatDate from "@/app/lib/formatDate";
+import { PortableText } from "next-sanity";
+import { renderComponents } from "@/sanity/lib/renderComponents";
 
-export default async function ProjectPage() {
-	return <Container></Container>;
+type Props = {
+	params: Promise<{ slug: string }>;
+};
+
+export default async function ProjectPage({ params }: Props) {
+	const { slug } = await params;
+	const project = await fetchProjectBySlug(slug);
+
+	if (!project) {
+		notFound();
+	}
+
+	return (
+		<Container
+			maxWidth='lg'
+			sx={{ py: 8, display: "grid", gap: "clamp(3rem, 5vw, 5rem)", alignItems: "start", gridTemplateColumns: { xs: "1fr", md: "1fr 240px" } }}>
+			<Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+				<Typography variant='overline'>
+					{project.technologies[0]} {formatDate(project.publishedAt)}
+				</Typography>
+				<Typography variant='h1'>{project.title}</Typography>
+				<Typography
+					variant='body1'
+					sx={{
+						fontSize: "20px",
+						fontWeight: 300,
+						maxWidth: "680px",
+						borderBottom: "1px solid",
+						borderColor: "divider",
+						paddingBottom: 4,
+						textWrap: "pretty",
+					}}>
+					{project.description}
+				</Typography>
+				<Box>
+					<PortableText value={project.content} components={renderComponents} />
+				</Box>
+			</Box>
+			{/* TOC GOES HERE */}
+			<Box>TOC Here</Box>
+		</Container>
+	);
 }
